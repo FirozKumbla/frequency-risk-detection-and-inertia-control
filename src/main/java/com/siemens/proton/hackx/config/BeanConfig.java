674dc7b30352707544712b9b4bbf75721815b885
z004ue7v <mayank-gupta@siemens.com>
2025-07-29 20:42:33 +0530

    allowed cross

--- src/main/java/com/siemens/proton/hackx/config/BeanConfig.java
+++ src/main/java/com/siemens/proton/hackx/config/BeanConfig.java
@@ -3,6 +3,8 @@ package com.siemens.proton.hackx.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BeanConfig {
@@ -11,5 +13,17 @@ public class BeanConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*") // Allow POST, PUT, DELETE, etc.
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
