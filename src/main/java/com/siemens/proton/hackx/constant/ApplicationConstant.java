070928f610afbc61f96c0c2e5fbc4d1803b044ff
z004ue7v <mayank-gupta@siemens.com>
2025-07-31 05:43:54 +0530

    updated

--- src/main/java/com/siemens/proton/hackx/constant/ApplicationConstant.java
+++ src/main/java/com/siemens/proton/hackx/constant/ApplicationConstant.java
@@ -5,5 +5,4 @@ public class ApplicationConstant {
     public static final String CURRENT_WEATHER_API_URI = "current.json";
     public static final String FORECAST_WEATHER_API_URI = "forecast.json";
     public static final String WEATHER_API_KEY = "dba599ff606741a3b7922729252907";
-    public static final String AI_API_KEY = "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";
 }
